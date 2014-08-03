c3f5178bdbae6570ff8d9694f388d0977f3d757b
bruno rasic <brunorasic@outlook.com>
2014-08-03 15:05:39 -0500

    Add elpy to init-python-mode.el

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -3,5 +3,15 @@
 		("SConscript\\'" . python-mode))
               auto-mode-alist))
 
+(require-package 'elpy)
+(elpy-enable)
+;;(elpy-clean-modeline)
+(elpy-use-ipython)
+(defalias 'workon 'pyvenv-workon)
+;; fixing a key binding bug in elpy
+;;(define-key yas-minor-mode-map (kbd "C-c k") 'yas-expand)
+;; fixing another key binding buf in iedit mode
+;;(define-key global-map (kbd "C-c o") 'iedit-mode)
+(setq elpy-rpc-backend "jedi")
 
 (provide 'init-python-mode)
