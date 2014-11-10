eeac232629069d7d659e3217478d66cfe3945017
brunor <brunor>
2014-11-10 16:09:22 -0600

    fix init-theme.el

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -6,7 +6,7 @@
 ;; If you don't customize it, this is the theme you get.
 ;;(setq-default custom-enabled-themes '(solarized-dark))
 ;;(add-to-list 'custom-theme-load-path "~/.emacs.d/site-lisp/emacs-color-theme-solarized")
-;;(load-theme 'solarized-dark t)
+(load-theme 'solarized-dark t)
 
 
 (provide 'init-themes)
